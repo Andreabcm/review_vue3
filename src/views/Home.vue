b3a5a60529456fd6277816f94ab9131af25fad43
unknown <andreabcm16@gmail.com>
2021-12-16 13:25:16 +0100

    passing the counter component to reactive with toRefs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <hello-world/>
   <hr/>
   <counter/>
+  <hr/>
+  <reactive-counter/>
 </template>
 
 <script>
 import HelloWorld from "@/components/First_HelloWorld"
 import Counter from "@/components/Second_SimpleCounter"
+import ReactiveCounter from "@/components/Third_ReactiveCounter"
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
     Counter,
+    ReactiveCounter,
   }
 }
 </script>
